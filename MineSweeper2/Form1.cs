8f59f05c59f77d30da2d23ed29f7fa68cba795e9
unknown <kipmap123@gmail.com>
2021-04-30 17:00:49 +0700

    add folder

---
+++ MineSweeper2/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var setButton = new MakeButton[9, 9];
+
+            int[,] ValueThisLocationArray = new int[9, 9];
+
+
+
+            for (int i = 0; i < 9; i++)
+            {
+
+                for (int j = 0; j < 9; j++)
+                {
+
+                    ValueThisLocationArray[i, j] = 0;
+
+                }
+
+            }
+
+            for (int LocationX = 0; LocationX < 9; LocationX++)
+            {
+
+                for (int LocationY = 0; LocationY < 9; LocationY++)
+                {
+
+                    setButton[LocationX, LocationY] = new MakeButton();
+
+                    setButton[LocationX, LocationY].Location = new System.Drawing.Point(LocationX * 30, LocationY * 30);
+
+                    setButton[LocationX, LocationY].Size = new System.Drawing.Size(30, 30);
+
+                    setButton[LocationX, LocationY].isOpen = true;
+
+
+
+                    this.Controls.Add(setButton[LocationX, LocationY]);
+
+
+                }
+
+            }
+
+            int IncreBoomInForm = 0;
+            //Limit Boom in Window Form 12
+            //And Random X, Y in Location Button
+            while (IncreBoomInForm < 5)
+            {
+
+                int RandomLocation = new Random().Next(9 * 9);
+                int r = RandomLocation / 9;
+                int c = RandomLocation % 9;
+                if (!setButton[r, c].isBoom)
+                {
+
+                    setButton[r, c].isBoom = true;
+                    IncreBoomInForm++;
+
+                    ValueThisLocationArray[r, c] = 9;
+                    for (int ToaDoX = r - 1; ToaDoX <= r + 1; ToaDoX++)
+                    {
+
+                        for (int ToaDoY = c - 1; ToaDoY <= c + 1; ToaDoY++)
+                        {
+                            //Kiem tra cac toa do xung quanh
+                            if ((ToaDoX < 9 & ToaDoY < 9) & (ToaDoX >= 0 & ToaDoY >= 0) & !(ToaDoX == r & ToaDoY == c))
+                            {
+
+
+                                ValueThisLocationArray[ToaDoX, ToaDoY]++;
+
+
+                            }
+
+                        }
+
+
+                    }
+
+
+
+
+                }
+
+
+            }
+
+            #region gán giá trị cho từng nút theo tọa độ
+            for (int i = 0; i < 9; i++)
+            {
+
+
+                for (int j = 0; j < 9; j++)
+                {
+
+
+                    setButton[i, j].BoomAround = ValueThisLocationArray[i, j];
+
+
+                }
+
+
+
+            }
+            #endregion
+
+
+            for(int i = 0; i < 9; i++)
+            {
+
+                for(int j = 0; j < 9; j++)
+                {
+
+                    if(setButton[i, j].BoomAround == 0 && setButton[i, j].isOpen == true)
+                    {
+
+                        DeQuy(i, j);
+
+                    }
+
+                }
+
+            }
+
+            void DeQuy(int LocationXclicked, int LocationYclicked)
+            {
+
+                for (int ToaDoX = LocationXclicked - 1; ToaDoX <= LocationXclicked + 1; ToaDoX++)
+                {
+
+                    for (int ToaDoY = LocationYclicked - 1; ToaDoY <= LocationYclicked + 1; ToaDoY++)
+                    {
+                        
+                        if ((ToaDoX < 9 & ToaDoY < 9) & (ToaDoX >= 0 & ToaDoY >= 0) & !(ToaDoX == LocationXclicked && ToaDoY == LocationYclicked))
+                        {
+
+
+                            setButton[ToaDoX, ToaDoY].Open();
+
+                            if(setButton[ToaDoX, ToaDoY].BoomAround == 0)
+                            {
+
+                                /*DeQuy(ToaDoX, ToaDoY);*/
+
+                            }
+                            
+
+
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ MineSweeper2/MakeButton.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MineSweeper2
+{
+    class MakeButton : Button
+    {
+
+        public bool isBoom = false;
+        public int BoomAround = 0;
+        public bool isOpen;
+
+        public MakeButton()
+        {
+
+            this.MouseDown += new MouseEventHandler(ButtonClicked);
+
+        }
+
+        public void ButtonClicked(object sender, MouseEventArgs e)
+        {
+
+            Open();
+
+            MakeButton btn = sender as MakeButton;
+            btn.isOpen = false;
+
+        }
+
+
+        public void Open()
+        {
+            
+            if (isBoom)
+            {
+
+                BOOM();
+
+            }
+            else
+            {
+
+                switch (BoomAround)
+                {
+
+                    case 0:
+                        isBoomAround0();
+                        break;
+
+                    case 1:
+                        isBoomAround1();
+                        break;
+
+                    case 2:
+                        isBoomAround2();
+                        break;
+
+                    case 3:
+                        isBoomAround3();
+                        break;
+
+                    case 4:
+                        isBoomAround4();
+                        break;
+
+                    case 5:
+                        isBoomAround5();
+                        break;
+
+                    case 6:
+                        isBoomAround6();
+                        break;
+
+                    case 7:
+                        isBoomAround7();
+                        break;
+
+                    case 8:
+                        isBoomAround8();
+                        break;
+
+
+
+
+                }
+
+                isOpen = false;
+
+
+            }
+
+
+
+        }
+
+
+
+
+        #region set up Boom
+        private void BOOM()
+        {
+
+            this.BackColor = Color.Red;
+            this.Text = "@";
+
+        }
+
+        private void isBoomAround0()
+        {
+
+            this.BackColor = Color.Gray;
+            this.Text = "";
+
+        }
+
+        private void isBoomAround1()
+        {
+
+            this.BackColor = Color.Blue;
+            this.Text = "1";
+
+        }
+
+        private void isBoomAround2()
+        {
+
+            this.BackColor = Color.Green;
+            this.Text = "2";
+
+        }
+
+        private void isBoomAround3()
+        {
+
+            this.BackColor = Color.Purple;
+            this.Text = "3";
+
+        }
+
+        private void isBoomAround4()
+        {
+
+            this.BackColor = Color.Orange;
+            this.Text = "4";
+
+        }
+
+        private void isBoomAround5()
+        {
+
+            this.BackColor = Color.OrangeRed;
+            this.Text = "5";
+
+        }
+
+        private void isBoomAround6()
+        {
+
+            this.BackColor = Color.OrangeRed;
+            this.Text = "6";
+
+        }
+
+        private void isBoomAround7()
+        {
+
+            this.BackColor = Color.OrangeRed;
+            this.Text = "7";
+
+        }
+
+        private void isBoomAround8()
+        {
+
+            this.BackColor = Color.OrangeRed;
+            this.Text = "8";
+
+        }
+
+        #endregion
+
+
+
+
+    }
+
+
+}
+
